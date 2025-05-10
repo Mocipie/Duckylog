d95c8a466b60383df71a717ed59f1380d0f49bc9
Mocipie <71799251+Mocipie@users.noreply.github.com>
2025-05-10 01:46:44 -0400

    Update mouseoff.ps1

--- mouseoff.ps1
+++ mouseoff.ps1
@@ -1,12 +1,9 @@
 Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
 
-# Inject C# to block mouse input
 Add-Type -TypeDefinition @"
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 
 public class MouseBlocker
 {
@@ -22,9 +19,6 @@ public class MouseBlocker
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-    [DllImport("user32.dll")]
-    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
     [DllImport("kernel32.dll")]
     private static extern IntPtr GetModuleHandle(string lpModuleName);
@@ -42,41 +36,19 @@ public class MouseBlocker
 
     private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
     {
-        return (IntPtr)1; // block all mouse input
+        return (IntPtr)1;
     }
 }
 "@ -ReferencedAssemblies "System.Windows.Forms"
 
-# Start mouse blocking
 [MouseBlocker]::Start()
 
-# Create an invisible form to trap the mouse
-$form = New-Object Windows.Forms.Form
-$form.WindowState = 'Normal'  # Not maximized
-$form.FormBorderStyle = 'None'
-$form.BackColor = 'Black'
-$form.TopMost = $true
-$form.KeyPreview = $true
-$form.ShowInTaskbar = $false
-
-# Hide the form (no visible UI)
-$form.Visible = $false
-
-# ESC to exit and clean up
-$form.Add_KeyDown({
-    if ($_.KeyCode -eq 'Escape') {
-        [System.Windows.Forms.Cursor]::Show()
-        [MouseBlocker]::Stop()
-        $form.Close()
+while ($true) {
+    if ([Console]::KeyAvailable) {
+        if ([Console]::ReadKey($true).Key -eq "Escape") {
+            [MouseBlocker]::Stop()
+            break
+        }
     }
-})
-
-# Trap and block mouse events within form too
-$form.Add_MouseClick({})
-$form.Add_MouseDown({})
-$form.Add_MouseMove({})
-$form.Add_MouseUp({})
-
-# Start form to block mouse while keeping it invisible
-[void]$form.ShowDialog()
-
+    Start-Sleep -Milliseconds 100
+}
